a1f00f291d95a3bbe006908e783101a8124654a2
andyzjc <83196324+andyzjc@users.noreply.github.com>
2021-09-07 18:06:41 -0400

    Draft of new validator
    New validator allows to check 1) missing field, 2) data type, 3) data dimension, 4) field requirement.

---
+++ python_validator_test.py
@@ -0,0 +1,69 @@
+import h5py as h5py
+import numpy as np
+import re
+import colorama
+from colorama import Fore, Style
+import sys
+
+def hdfgetdata(gID, field):
+    val = gID.get(field)
+    if val is None:
+        return val
+    if h5py.check_string_dtype(val.dtype):
+        # string
+        if val.len() == 1:
+            val = val[0].decode('ascii')
+            return val
+        else:
+            val2 = []
+            for x in val:
+                val2.append(x.decode('ascii'))
+            val2 = np.array(val2)
+            return val2
+    val = np.array(val)
+    if (val.ndim == 1 and len(val) == 1):
+        val = val[0]
+    return val
+
+def validate(filename, fileOut=None):
+    fileID = h5py.File(filename, 'r')
+
+    def getallnames(gID, lst):
+        if isinstance(gID, h5py.File):
+            Required = ["formatVersion", "nirs"]
+            RequiredIndex = [0, 0]
+            for child in gID:
+                if child in Required: # if nirs/formatversion in RequiredField, change RequiredField Index
+                    RequiredIndex[Required.index(child)] = 1
+                else:
+                    print(Fore.RED + "\tInvalid Group: " + )
+                getallnames(gID[child], lst)
+            # check if requiredFieldIndex has 0, if not, no missing field
+        elif isinstance(gID, h5py.Group):
+            # if gID is /nirs, requiredfield = ["metaDataTag","data","probe"]
+            # if gID is /data, requireField = ["dataTimeSeries","time","measurementList"]
+            # if gID is /measurementList, requiredField = [sourceIndex,detectorIndex,wavelengthIndex,dataType,dataTypeIndex]
+            # if gID is /stim, requiredField = [name,data]
+            # if gID is /aux, requiredField = [name,data,time]
+            for y in gID:
+                # if y in the requireField, change requirefield index
+                getallnames(gID[y], lst)
+            # check if requiredFieldIndex has 0, if not, no missing field
+        elif isinstance(gID, h5py.Dataset):
+            lst.append(gID.name)
+            # required/optional check
+            # datatype check
+            # dimension check
+        else:
+            return 0
+
+    lst = []
+    getallnames(fileID, lst)
+
+def main():
+    filename=sys.argv[1]
+    print(filename)
+    validate(filename)
+
+if __name__ == "__main__":
+    main()
