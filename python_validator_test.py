e6adc7ae96def21cd0074e87239c0aa07aebb518
andyzjc <83196324+andyzjc@users.noreply.github.com>
2021-09-09 00:53:45 -0400

    still developing

--- python_validator_test.py
+++ python_validator_test.py
@@ -25,74 +25,119 @@ def hdfgetdata(gID, field):
         val = val[0]
     return val
 
-def validate(filename, fileOut=None):
+def validate(filename, optionalList):
     fileID = h5py.File(filename, 'r')
 
-    def CheckRequiredIndex(Name,required,requiredIndex):
+    def getRequiredDataset(gID):
+        if 'measurementList' in gID.name:
+            required = ["sourceIndex", "detectorIndex", "wavelengthIndex", "dataType", "dataTypeIndex"]
+        elif 'data' in gID.name:
+            required = ["dataTimeSeries", "time", "measurementList"]
+        elif 'stim' in gID.name:
+            required = ["name", "data"]
+        elif 'aux' in gID.name:
+            required = ["name", "dataTimeSeries", "time"]
+        elif 'metaDataTags' in gID.name:
+            required = ["SubjectID", "MeasurementDate", "MeasurementTime", "LengthUnit", "TimeUnit", "FrequencyUnit"]
+        elif 'probe' in gID.name:
+            required = ["wavelengths"]
+        elif 'nirs' in gID.name:
+            required = ["metaDataTags", "data", "probe"]
+        else:
+            return 0
+        return required
+
+    def checkSpecialCase(required, requiredIndex, child):
+        if 'sourcePos2D' in child or 'detectorPos2D' in child:
+            if 'sourcePos2D' not in required and 'detectPos2D' not in required:
+                required.append("sourcePos2D")
+                requiredIndex.append(0)
+                required.append("detectorPos2D")
+                requiredIndex.append(0)
+            childForCheck = child
+        elif 'sourcePos3D' in child or 'detectorPos3D' in child:
+            if 'sourcePos3D' not in required and 'detectPos3D' not in required:
+                required.append("sourcePos3D")
+                requiredIndex.append(0)
+                required.append("detectorPos3D")
+                requiredIndex.append(0)
+            childForCheck = child
+        elif 'landmarkPos' in child:
+            childForCheck = child
+        else:
+            childForCheck = ''.join(i for i in child if not i.isdigit())
+        return required, requiredIndex, childForCheck
+
+    def checkRequiredIndex(Name, required, requiredIndex):
         if 0 in requiredIndex:  # check if requiredIndex has 0, if so, append the name
             for i in range(len(required)):
                 if requiredIndex[i] == 0:
                     missingList.append(Name + '/' + required[i])
 
-    def getallnames(gID, completeList, missingList):
+    def checkGroupChild(gID, required):
+        requiredIndex = [0] * len(required)
+        for child in gID:
+            requireFlag = False
+            if any(chr.isdigit() for chr in child):
+                [required, requiredIndex, childForCheck] = checkSpecialCase(required, requiredIndex, child)
+            else:
+                childForCheck = child
+            if childForCheck in required:  # if child in RequiredField, change RequiredIndex
+                requiredIndex[required.index(childForCheck)] = 1
+                requireFlag = True
+            printEverything(gID, child, optionalList, requireFlag)
+            getAllNames(gID[child])
+        checkRequiredIndex(gID.name, required, requiredIndex)
+
+    def printEverything(gID, child, optionalList, requireFlag):
+        if requireFlag == True:
+            if isinstance(gID[child], h5py.Dataset):
+                if "stim" in gID.name or "aux" in gID.name:
+                    print(Fore.WHITE + '\t' + gID.name + '/' + child)
+                    print(Fore.GREEN + '\t\tRequired Dataset When Parent Group ' + gID.name + ' Presents')
+                else:
+                    print(Fore.WHITE + '\t' + gID.name + '/' + child)
+                    print(Fore.GREEN + '\t\tRequired Dataset')
+            if isinstance(gID[child], h5py.Group):
+                print(Fore.MAGENTA + gID[child].name)
+                print(Fore.GREEN + '\tRequired Indexed Group')
+        else:
+            OptionalFlag = False
+            for x in optionalList:
+                if re.match(x, gID[child].name):
+                    if isinstance(gID[child], h5py.Dataset):
+                        print(Fore.WHITE + '\t' + gID.name + '/' + child)
+                        print(Fore.BLUE + '\t\tOptional Dataset')
+                        OptionalFlag = True
+                        break
+                    if isinstance(gID[child], h5py.Group):
+                        print(Fore.MAGENTA + gID[child].name)
+                        print(Fore.BLUE + '\tOptional Indexed Group')
+                        OptionalFlag = True
+                        break
+            if OptionalFlag == False:
+                if isinstance(gID[child], h5py.Dataset):
+                    if 'metaDataTags' in gID.name:
+                        print(Fore.WHITE + '\t' + gID.name + '/' + child)
+                        print(Fore.YELLOW + '\t\tUser Defined optional Dataset')
+                    else:
+                        print(Fore.WHITE + '\t' + gID.name + '/' + child)
+                        print(Fore.RED + '\t\tInvalid Dataset')
+                        invalidDatasetList.append(gID.name)
+                if isinstance(gID[child], h5py.Group):
+                    print(gID.name)
+                    print(Fore.RED + '\tInvalid Indexed Group')
+                    invalidGroupList.append(gID.name)
+
+    def getAllNames(gID):
         if isinstance(gID, h5py.File):
             required = ["formatVersion", "nirs"]
-            requiredIndex = [0] * len(required)
-            for child in gID:
-                childNoNum = ''.join(i for i in child if not i.isdigit())
-                if childNoNum in required:  # if child in RequiredField, change RequiredIndex
-                    requiredIndex[required.index(childNoNum)] = 1
-                else:
-                    print(Fore.RED + "\tInvalid Indexed Group: " + str(child))
-                getallnames(gID[child], completeList, missingList)
-            CheckRequiredIndex(gID.name, required, requiredIndex)
+            checkGroupChild(gID, required)
+
         elif isinstance(gID, h5py.Group):
-            print(gID.name)
-            if 'measurementList' in gID.name:
-                required = ["sourceIndex", "detectorIndex", "wavelengthIndex", "dataType", "dataTypeIndex"]
-            elif 'data' in gID.name:
-                required = ["dataTimeSeries", "time", "measurementList"]
-            elif 'stim' in gID.name:
-                required = ["name", "data"]
-            elif 'aux' in gID.name:
-                required = ["name", "dataTimeSeries", "time"]
-            elif 'metaDataTags' in gID.name:
-                required = ["SubjectID", "MeasurementDate", "MeasurementTime", "LengthUnit", "TimeUnit", "FrequencyUnit"]
-            elif 'probe' in gID.name:
-                required = ["wavelengths"]
-            elif 'nirs' in gID.name:
-                required = ["metaDataTags", "data", "probe"]
-            else:
-                print(Fore.RED + "\tInvalid Group: " + str(gID.name))
-                return 0
-            requiredIndex = [0] * len(required)
+            required = getRequiredDataset(gID)
+            checkGroupChild(gID, required)
 
-            for child in gID:
-                if 'sourcePos2D' in child or 'detectorPos2D' in child:
-                    if 'sourcePos2D' not in required and 'detectPos2D' not in required:
-                        required.append("sourcePos2D")
-                        requiredIndex.append(0)
-                        required.append("detectorPos2D")
-                        requiredIndex.append(0)
-                    if child in required:
-                        requiredIndex[required.index(child)] = 1
-                elif 'sourcePos3D' in child or 'detectorPos3D' in child:
-                    if 'sourcePos3D' not in required and 'detectPos3D' not in required:
-                        required.append("sourcePos3D")
-                        requiredIndex.append(0)
-                        required.append("detectorPos3D")
-                        requiredIndex.append(0)
-                    if child in required:
-                        requiredIndex[required.index(child)] = 1
-                else:
-                    childNoNum = ''.join(i for i in child if not i.isdigit())
-                    if childNoNum in required:  # if child in RequiredField, change RequiredIndex
-                        requiredIndex[required.index(childNoNum)] = 1
-                    else: # check if optional
-
-
-                getallnames(gID[child], completeList, missingList)
-            CheckRequiredIndex(gID.name, required, requiredIndex)
         elif isinstance(gID, h5py.Dataset):
             completeList.append(gID.name)
             # datatype check
@@ -102,13 +147,33 @@ def validate(filename, fileOut=None):
 
     completeList = []
     missingList = []
-    getallnames(fileID, completeList, missingList)
+    invalidGroupList = []
+    invalidDatasetList = []
+
+    getAllNames(fileID)
+
     print(missingList)
+    print(invalidGroupList)
+    print(invalidDatasetList)
 
 def main():
+    optionalList = ["/nirs\d*/data\w*/measurementList\d*/wavelengthActual",
+                "/nirs\d*/data\w*/measurementList\d*/wavelengthEmissionActual",
+                "/nirs\d*/data\d*/measurementList\d*/dataTypeLabel",
+                "/nirs\d*/data\w*/measurementList\d*/sourcePower",
+                "/nirs\d*/data\w*/measurementList\d*/detectorGain",
+                "/nirs\d*/data\w*/measurementList\d*/moduleIndex",
+                "/nirs\d*/data\w*/measurementList\d*/sourceModuleIndex",
+                "/nirs\d*/data\w*/measurementList\d*/detectorModuleIndex", 
+                "/nirs\d*/probe/wavelengthsEmission","/nirs\d*/probe/frequencies",
+                "/nirs\d*/probe/timeDelays", "/nirs\d*/probe/timeDelayWidths", "/nirs\d*/probe/momentOrders",
+                "/nirs\d*/probe/correlationTimeDelays", "/nirs\d*/probe/correlationTimeDelayWidths",
+                "/nirs\d*/probe/sourceLabels", "/nirs\d*/probe/detectorLabels", "/nirs\d*/probe/landmarkPos2D",
+                "/nirs\d*/probe/landmarkPos3D", "/nirs\d*/probe/landmarkLabels", "/nirs\d*/probe/useLocalIndex",
+                "/nirs\d*/aux\d*/timeOffset", "/nirs\d*/stim\d*/dataLabels","/nirs\d*/stim\d*","/nirs\d*/aux\d*"]
     filename=sys.argv[1]
-    print(filename)
-    validate(filename)
+    print(Fore.MAGENTA + filename)
+    validate(filename,optionalList)
 
 if __name__ == "__main__":
     main()
